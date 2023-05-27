6b27dd16621358d5b998284d593e26c73557111b
Valerie Besch <valeriebesch@mbp-de-valerie.home>
2023-05-27 11:05:27 +0200

    add stripe

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const morgan = require("morgan");
 const cloudinary = require("cloudinary").v2;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 
 mongoose.connect(process.env.MOGODB_URI);
 
